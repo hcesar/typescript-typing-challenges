9f94f24ba6c258f20fcb56a52b3ba58f78acf246
Henrique Cesar <fhenrique@gmail.com>
2021-04-26 23:05:57 +0200

    nochangelog: 2st challenge

---
+++ src/2-entity-mutation-observer/README.md
@@ -0,0 +1,7 @@
+# Entity Mutation Observer.
+
+Expected code errors outline:
+![Code error outline](./expected1.png?raw=true "Code error outline")
+
+Expected code errors output:
+![Code errors output](./expected2.png?raw=true "Code errors output")
---
+++ src/2-entity-mutation-observer/code.ts
@@ -0,0 +1,37 @@
+type PropEventSource<T> = never; // FIX ME
+
+class PersonaMutationObserver<T> {
+  constructor(obj: T, observer: PropEventSource<T>) {}
+}
+
+type Person = {
+  id: number;
+  name: string;
+  birthDate: Date;
+  country: string;
+};
+
+const person: Person = {
+  id: 1,
+  name: "Paul",
+  birthDate: new Date("2000-01-01"),
+  country: "Spain",
+};
+
+//OK (callback should be infered to correct type or can be explicity declared)
+const observer = new PersonaMutationObserver(person, {
+  onNameChanged: (name) => console.log(name.toLocaleLowerCase()),
+  onBirthDateChanged: (date) => console.log(date.toLocaleDateString()),
+  onCountryChanged: (value: string) => console.log(value.toUpperCase()),
+});
+
+//Error: there is no property named xpto in Person type
+const observer2 = new PersonaMutationObserver(person, {
+  onNameChanged: (value) => console.log(value),
+  onXptoChanged: (value) => console.log(value),
+});
+
+//Error: callback type is wrong
+const observer3 = new PersonaMutationObserver(person, {
+  onNameChanged: (value: Date) => console.log(value),
+});
